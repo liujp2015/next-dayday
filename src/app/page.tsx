34b365dddfbab444c916188d2b85918e28420fae
liujunpeng <392303104@qq.com>
2025-07-01 14:34:05 +0800

    adding second part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      {/** 背景颜色*/}
-      <div className="w-full bg-gray-400 pt-20">
-        <div className="w-full max-w-screen-xl flex items-center px-4 pb-10 m-auto lg:px-10  lg:pb-20">
+      {/** 最外层容器设置背景颜色*/}
+      <div className=" bg-gray-400 pt-20">
+        {/** 内层容器*/}
+        <div className="max-w-screen-xl flex items-center px-4 pb-10 m-auto lg:px-10  lg:pb-20">
           <div className="w-full lg:w-1/2">
             <h1 className="text-center text-3xl pt-10 lg:text-5xl lg:pt-20 lg:text-left">
               每日朋友圈
@@ -27,6 +28,25 @@ export default function Home() {
               height={500} // 使用 viewBox 的高度
               className="w-full max-w-screen-md m-auto" // 宽度撑满，高度自适应
             />
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-screen-xl  lg:px-10 px-4 py-20 flex lg:flex-row flex-col-reverse m-auto items-center">
+        <div className="w-full lg:w-1/2 lg:pr-20">
+          <Image
+            src="/block-1.svg"
+            alt="示例"
+            width={500} // 使用 viewBox 的宽度
+            height={500} // 使用 viewBox 的高度
+            className="w-full max-w-screen-md m-auto" // 宽度撑满，高度自适应
+          />
+        </div>
+        <div className="w-full lg:w-1/2 lg:text-left text-center">
+          <h2 className="text-4xl">轻松编辑，简单操作</h2>
+
+          <div className="mt-8 text-gray-600">
+            拒绝千篇一律，让你的朋友圈与众不同...
           </div>
         </div>
       </div>
