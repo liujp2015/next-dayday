5564933bfdb6dfe55a0ed7ddd69137f9637ca022
liujunpeng <392303104@qq.com>
2025-07-01 10:53:03 +0800

    init project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div> 我是head</div>
         {children}
+        <div> 我是feeter</div>
       </body>
     </html>
   );
