54d23491eee9cda97886d4954aac89de86c02740
liujunpeng <392303104@qq.com>
2025-07-04 19:03:10 +0800

    expired time of session

--- src/auth.ts
+++ src/auth.ts
@@ -8,6 +8,10 @@ const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
+  session: {
+    strategy: "jwt",
+    maxAge: 7 * 24 * 60 * 60, // 会话有效期（秒），默认 30 天
+    updateAge: 24 * 60 * 60, // 会话更新时间（秒），默认 24 小时
+  },
   ...authConfig,
 });
